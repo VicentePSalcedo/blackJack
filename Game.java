7126b27b2b241248ffa518ca01d462dbe9f93193
Vicente Salcedo <vicentepsalcedo@gmail.com>
2023-07-09 16:01:21 -0400

    working on the user interaction so I can start testing soon

--- Game.java
+++ Game.java
@@ -1,12 +1,51 @@
-public class Game{
-    public static void main(String[] args){
-        Shoe shoe = new Shoe();
-    }
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Game{
+    static Player player = new Player();
+    public static void main(String[] args){
+
+        Shoe shoe = new Shoe();
+        Dealer dealer = new Dealer();
+
+        System.out.println("Welcome to BlackJack.");
+        //Game setup by the player
+        System.out.println("How big a bankroll do you have today?");
+        Scanner userIn = new Scanner(System.in);
+        player.setBankroll(userIn.nextInt());
+        printBankRoll();
+    }
+    public void hit(){
+    }
+    public static void printBankRoll(){
+        NumberFormat dollar = NumberFormat.getCurrencyInstance(Locale.US);
+        String bankString = dollar.format(player.getBankroll());
+        System.out.println(bankString);
+    }
+}
+
+class Dealer{
+    ArrayList<Card> hand = new ArrayList<Card>();
+}
+
+class Player{
+    private int bankroll;
+    int currentBet;
+    ArrayList<Card> hand = new ArrayList<Card>();
+    public void setBankroll(int userIn){
+        this.bankroll = userIn;
+    }
+    public int getBankroll(){
+        return this.bankroll;
+    }
 }
 
 class Shoe{
     Card cards[] = new Card[312];
+    int totalCards = 312;
     public Shoe(){
         int decks = 0;
         int cardCount = 0;
@@ -14,11 +53,26 @@ class Shoe{
             for(Suit suit : Suit.values()){
                 for(Rank rank : Rank.values()){
                     Card card = new Card(suit, rank);
-                    cards[cardCount] = card;
+                    this.cards[cardCount] = card;
                     cardCount++;
                 } 
             }
             decks++;
+        }
+        shuffle();
+    }
+    public void shuffle(){
+        Random rand = new Random();
+        for(int i = 0; i < this.cards.length; i++){
+            int randomIndexToSwap = rand.nextInt(this.cards.length);
+            Card temp = this.cards[randomIndexToSwap];
+            this.cards[randomIndexToSwap] = this.cards[i];
+            this.cards[i] = temp;
+        }
+    }
+    public void printShoe(){
+        for(Card card : this.cards){
+            System.out.println(card.rank + " of " + card.suit);
         }
     }
 }
