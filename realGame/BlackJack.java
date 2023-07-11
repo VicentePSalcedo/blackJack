62abee4e58fcc82280775c44c5cda443e5f1bd18
Vicente Salcedo <vicentepsalcedo@gmail.com>
2023-07-11 15:41:45 -0400

    implemented wager state and dealing state

--- realGame/BlackJack.java
+++ realGame/BlackJack.java
@@ -1,33 +1,115 @@
 package realGame;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class BlackJack{
     public static void main(String[] args){
         System.out.println("Welcome to BlackJack.");
         Game game = new Game();
-        game.run();
+        while(!game.IsComplete()){
+            game.run();
+        }
+        System.out.println("See ya next time!");
     }
 }
 class Game{
+    enum State{
+        WAGERING,
+        DEALING,
+        PLAYERTURN,
+        DEALERTURN,
+        LOSS,
+        PUSH,
+        WIN,
+        ATM,
+        QUIT;
+    }
+    State state;
     Shoe shoe;
     ArrayList<Card> player;
     ArrayList<Card> dealer;
-    int bankdRoll;
+    int bankRoll, bet;
     public Game(){
         this.shoe = new Shoe();
         this.player = new ArrayList<Card>();
         this.dealer = new ArrayList<Card>();
-        this.bankdRoll = 5000;
+        this.bankRoll = 5000;
+        this.bet = 0;
+        this.state = State.WAGERING;
     }
     public void run(){
-        
+        switch(state){
+            case WAGERING:
+                Scanner userInput = new Scanner(System.in);
+                do {
+                    System.out.println("How much would you like to wager?[Min:$10-Max:$" + bankRoll + "]");
+                    while(!userInput.hasNextInt()){
+                        System.out.println("Please enter a positive integer less than $" + bankRoll);
+                        userInput.next();
+                    }
+                    bet = userInput.nextInt();
+                } while ((bet < 10 || bet > bankRoll) || (bet%10) != 0);
+                System.out.println("You bet $" + bet + ".");
+                state = State.DEALING;
+                break;
+            case DEALING:
+                hitPlayer();
+                hitDealer();
+                hitPlayer();
+                hitDealer();
+                state = State.PLAYERTURN;
+                break;
+            case PLAYERTURN:
+                printHand(player);
+                System.out.println("PlaterTurn State isn't working yet. Quitint out.");
+                state = State.QUIT;
+                break;
+            case DEALERTURN:
+                System.out.println("DealerTunr State isn't working yet. Quitint out.");
+                state = State.QUIT;
+                break;
+            case LOSS:
+                System.out.println("Loss State isn't working yet. Quitint out.");
+                state = State.QUIT;
+                break;
+            case PUSH:
+                System.out.println("Push State isn't working yet. Quitint out.");
+                state = State.QUIT;
+                break;
+            case WIN:
+                System.out.println("Win State isn't working yet. Quitint out.");
+                state = State.QUIT;
+                break;
+            case ATM:
+                System.out.println("Atm State isn't isn't working yet. Quitint out.");
+                state = State.QUIT;
+                break;
+            case QUIT:
+                break;
+        }
+    }
+    public void printHand(ArrayList<Card> hand){
+        for(Card card : hand){
+            System.out.println(card.rank + " of " + card.suit);
+        }
+    }
+    public void hitPlayer(){
+        player.add(shoe.cards[shoe.topOfDeck]);
+        shoe.topOfDeck--;
+    }
+    public void hitDealer(){
+        dealer.add(shoe.cards[shoe.topOfDeck]);
+        shoe.topOfDeck--;
+    }
+    public boolean IsComplete(){
+        return this.state == State.QUIT;
     }
 }
 //we store the cards this way so that people can count cards in this game
 class Shoe{
     Card cards[] = new Card[312];
-    int totalCards = 312;
+    int topOfDeck;
     public Shoe(){
         int decks = 0;
         int cardCount = 0;
@@ -51,6 +133,7 @@ class Shoe{
             this.cards[randomIndexToSwap] = this.cards[i];
             this.cards[i] = temp;
         }
+        this.topOfDeck = 311;
     }
     public void printShoe(){
         for(Card card : this.cards){
