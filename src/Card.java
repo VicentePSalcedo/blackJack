3768194aeb002edb8cca42393240ddff5bca02cd
Brandon Araujo <brandonmtt125@hotmail.com>
2023-07-23 15:43:56 -0400

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -3,9 +3,11 @@ package src;
 public class Card {
     Suit suit;
     Rank rank;
+    int rankInt;
     public Card(Suit suit, Rank rank){
         this.suit = suit;
         this.rank = rank;
+        this.rankInt = rankInt;
     }
     public enum Suit {
         Spades,
